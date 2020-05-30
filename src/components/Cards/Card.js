e88bbfeb3f135f2b9a716582af4bafc2109ba723
Lucas Carvalho Honorio <caldoxgamer@gmail.com>
2020-05-30 14:40:28 -0300

    Improvements

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -4,7 +4,7 @@ export default (props) => (
     <div id="card" className="p-4 d-flex justify-content-center align-items-center">
         <div>
             <h2 id='card-title' className="text-center font-weight-bold">{props.porcent}</h2>
-            <p className="text-center">{props.text}</p>
+            <p className="text-center font-weight-bold">{props.text}</p>
         </div>
         <div id="footer" style={{backgroundColor:props.color}}/>
     </div>
