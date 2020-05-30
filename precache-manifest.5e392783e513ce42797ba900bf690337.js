f026530019ba2ed8f089f4af7946abd9f8223236
Lucas Carvalho Honorio <caldoxgamer@gmail.com>
2020-05-30 13:35:59 -0300

    Updates

---
+++ precache-manifest.5e392783e513ce42797ba900bf690337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b77fd0dc7f4d7599aaa1b9e276e106",
+    "url": "/landing-page-react/index.html"
+  },
+  {
+    "revision": "3b0a6a2bfd39bc046644",
+    "url": "/landing-page-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a47cbe4aaaf454f411a6",
+    "url": "/landing-page-react/static/css/main.00ac462e.chunk.css"
+  },
+  {
+    "revision": "3b0a6a2bfd39bc046644",
+    "url": "/landing-page-react/static/js/2.c9b21e0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing-page-react/static/js/2.c9b21e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47cbe4aaaf454f411a6",
+    "url": "/landing-page-react/static/js/main.44c52c37.chunk.js"
+  },
+  {
+    "revision": "f5f233c09fe7010f8148",
+    "url": "/landing-page-react/static/js/runtime-main.8ef2605c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cefcbd260b30ac94491e10c772b9b564.js"
+  "/landing-page-react/precache-manifest.5e392783e513ce42797ba900bf690337.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/landing-page-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
