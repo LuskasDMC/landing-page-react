4105b4c34966e068b2281af2017a379a3968b29b
Lucas Carvalho Honorio <caldoxgamer@gmail.com>
2020-05-29 23:57:11 -0300

    Updates

---
+++ precache-manifest.c5c1ff36a4463d9109cc9f069480ae89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e88619a43399c1a597a1ab9c3a6f302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b0a6a2bfd39bc046644",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a246d1d3400bac29d7fc",
+    "url": "/static/css/main.7f93f14a.chunk.css"
+  },
+  {
+    "revision": "3b0a6a2bfd39bc046644",
+    "url": "/static/js/2.c9b21e0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9b21e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a246d1d3400bac29d7fc",
+    "url": "/static/js/main.0db916e2.chunk.js"
+  },
+  {
+    "revision": "a0cea74a29426add5362",
+    "url": "/static/js/runtime-main.4a373ec1.js"
+  }
+]);
